f397e8073e59fc0f7da32a575cd5e84d14c5653c
mouerj <joseph.mouer@icloud.com>
2016-03-02 15:43:07 -0600

    merged all working branches

--- GalleryandMuseumFinder/DetailViewController.swift
+++ GalleryandMuseumFinder/DetailViewController.swift
@@ -15,25 +15,17 @@ import MapKit
 class DetailViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
     
     @IBOutlet weak var nameLabel: UILabel!
-  
-    @IBOutlet weak var openLabel: UILabel!
-    
+    @IBOutlet weak var addressTextView: UITextView!
+    @IBOutlet weak var phoneTextView: UITextView!
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var websiteTextView: UITextView!
-   
-    @IBOutlet weak var phoneNumber: UITextView!
-   
-    @IBOutlet weak var addressLabel: UITextView!
+    @IBOutlet weak var hoursTextView: UITextView!
     
     var detailArray = [Gallery]()
     
     var viaSegue: String!
     
-
-    
-    let photoURL = String("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRlAAAAs7_BywmwJ_Y5udZ77zewt8_APqZOu1rdqg7h5C_zI7gEO-hnFByVUuvBcAD1bhKiwa1IpCMACkmf037vztqnWOPBx0Ocrtdq8k3yk4RAg7UOPLj1PkQVy8sbSfUZ6UKeEO2hWefK1PHWgaSXX7_gfhIQDMYBM-V6E1wn6Yu-3Fe-bxoUKn6eM9SI8VatnSExK7hOwg9gCn8&key=AIzaSyDNopD2lCPhs0z-Uap3f8EPUt9R3gGjGjg")
-
+    var photoRef: String!
     
     let session1 = NSURLSession.sharedSession()
     let session2 = NSURLSession.sharedSession()
@@ -41,16 +33,92 @@ class DetailViewController: UIViewController, CLLocationManagerDelegate, GMSMapV
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = UIColor.grayColor()
+        
         self.detailPull()
         
-        print("making sure this works  \(self.viaSegue)")
-
-        if let url = NSURL(string: "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CmRdAAAAB84NH5OrUibk1rVmpSccMLOd706q-urUIjPNUput41lvdYIjPdg8cwFEojBT5sW4bViuM4bFBRAWimknEVoo-1J1wedx78o3a1UkHBDfuE5BTLypl9nK511CjtdBYOZLEhDM8YgNtBI9FGkva1oBxkP3GhSNvQm9uhkW1CrrEXYwuNr7YMhdag&key=AIzaSyDNopD2lCPhs0z-Uap3f8EPUt9R3gGjGjg") {
+   
+        
+    }
+    
+    func getDataFromUrl(url:NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void)) {
+        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            completion(data: data, response: response, error: error)
+            } .resume()
+    }
+    
+    
+    func detailPull () {
+        
+        let detailURL = String("https://maps.googleapis.com/maps/api/place/details/json?&key=AIzaSyDNopD2lCPhs0z-Uap3f8EPUt9R3gGjGjg&placeid=\(self.viaSegue)")
+        
+        let url1 = NSURL(string: detailURL)
+        
+        let detailTask = session1.dataTaskWithURL(url1!) { (data , response, error ) -> Void in
+            do {
+                
+                let jsonDict = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                    as! Dictionary<String, AnyObject>
+                let result = jsonDict["result"] as! Dictionary<String, AnyObject>
+                
+                self.imageView.image = UIImage(named: "")
+                
+                if let photos = result["photos"] as? NSArray {
+                    
+                    for photoDict in photos {
+                        self.photoRef = photoDict["photo_reference"] as? String
+                        
+                        self.photoPull()
+                        
+                    }
+                }
+                
+                
+                
+                print("RESULT: \(result)")
+                dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                    if let hoursDict = result["opening_hours"] as? Dictionary<String, AnyObject> {
+                    print("HOURS: \(hoursDict)")
+                    
+                    let hoursArray = hoursDict["weekday_text"] as? NSArray
+                        print("HOURS ARRAY: \(hoursArray)")
+                        
+                        
+                        self.hoursTextView.text = (hoursArray!.componentsJoinedByString("\r\n"))
+                        
+                    
+                    }
+                    self.nameLabel.text = result["name"] as? String
+                    self.addressTextView.text = result["formatted_address"] as? String
+                    self.addressTextView.editable = (false)
+                    self.addressTextView.dataDetectorTypes = UIDataDetectorTypes.All
+                    self.websiteTextView.text = result["website"] as? String
+                    self.phoneTextView.text = result["formatted_phone_number"] as? String
+                    self.phoneTextView.editable = (false)
+                    self.phoneTextView.dataDetectorTypes = UIDataDetectorTypes.All
+                    
+                }
+                
+            }
+            catch let error as NSError {
+                print("jsonError: \(error.localizedDescription)")
+            }
             
-           
-            getDataFromUrl(url, completion: { (data, response, error) -> Void in
+        }
+        
+        detailTask.resume()
+        
+    }
+    
+    
+    func photoPull () {
+        let photoUrl = String("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key=AIzaSyDNopD2lCPhs0z-Uap3f8EPUt9R3gGjGjg&photoreference=\(self.photoRef)")
+        
+        if let url2 = NSURL(string: photoUrl) {
+            
+            getDataFromUrl(url2, completion: { (data, response, error) -> Void in
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        self.imageView.image = UIImage(data: data!)
+                    self.imageView.image = UIImage(data: data!)
                     
                 })
                 
@@ -60,44 +128,5 @@ class DetailViewController: UIViewController, CLLocationManagerDelegate, GMSMapV
         
     }
     
-    func getDataFromUrl(url:NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void)) {
-        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
-            completion(data: data, response: response, error: error)
-        } .resume()
-    }
-    
-        func detailPull () {
-        
-        let detailURL = String("https://maps.googleapis.com/maps/api/place/details/json?&key=AIzaSyDNopD2lCPhs0z-Uap3f8EPUt9R3gGjGjg&placeid=\(self.viaSegue)")
-        
-        let url1 = NSURL(string: detailURL)
-        
-        let detailTask = session1.dataTaskWithURL(url1!) { (data , response, error ) -> Void in
-            do {
-
-                let jsonDict = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
-                    as! Dictionary<String, AnyObject>
-                let result = jsonDict["result"] as! Dictionary<String, AnyObject>
-                print("RESULT: \(result)")
-                dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                    self.nameLabel.text = result["name"] as? String
-                    self.addressLabel.text = result["formatted_address"] as? String
-                    self.addressLabel.editable = (false)
-                    self.addressLabel.dataDetectorTypes = UIDataDetectorTypes.All
-                    self.websiteTextView.text = result["website"] as? String
-                    self.phoneNumber.text = result["formatted_phone_number"] as? String
-                    self.phoneNumber.editable = (false)
-                    self.phoneNumber.dataDetectorTypes = UIDataDetectorTypes.All
-                    self.openLabel.text = result["weekday_text"] as? String
-                    
-                    
-                }
-            }
-            catch let error as NSError {
-                print("jsonError: \(error.localizedDescription)")
-            }
-        }
-        detailTask.resume()
-    }
-   
 }
+
