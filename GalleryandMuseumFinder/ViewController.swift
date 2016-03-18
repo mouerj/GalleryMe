3830aea6171fcd9ce3d0bb0787666e4d29878f5f
mouerj <joseph.mouer@icloud.com>
2016-03-18 12:48:15 -0400

    Added interim user initiated city search results to code

--- GalleryandMuseumFinder/ViewController.swift
+++ GalleryandMuseumFinder/ViewController.swift
@@ -43,6 +43,8 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegat
     
     var userLoc = String()
     
+    var userLoc2 = String()
+    
     var locationManager = CLLocationManager()
     
     var didFindMyLocation = false
@@ -163,13 +165,7 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegat
                     self.data_request()
                     print("I'm in \(self.userLoc)")
                     
-        //CHANGE LOCATION ON MAP??
-//                } else if self.changeLocText.isEmpty == false {
-//                    let pm = placemarks![0] as! CLPlacemark
-//                    self.changeLocText = self.userLoc
-//                    self.changeLocText = pm.locality!
-//                    self.data_request()
-                
+                    
                 } else {
                     print("Problem with the data received from geocoder")
                 }
@@ -220,8 +216,8 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegat
                     }
                 }
                 dispatch_async(dispatch_get_main_queue()) { () -> Void in
-
-                self.tableView.reloadData()
+                    
+                    self.tableView.reloadData()
                 }
             }
                 
@@ -392,17 +388,18 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegat
         }
     }
     
-    //CHANGE USER LOCATION 
+    //CHANGE USER LOCATION
     
     @IBAction func onChangeLocationTapped(sender: UIBarButtonItem) {
         var inputTextField: UITextField!
         let changeLocPrompt = UIAlertController(title: "Enter New Location", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
         changeLocPrompt.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
         changeLocPrompt.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
-            self.userLoc = inputTextField.text!
             print("\(inputTextField?.text)")
-            print("\(self.userLoc)")
-            self.data_request()
+            print("\(self.userLoc2)")
+            self.userLoc2 = inputTextField.text!
+            self.data_request2()
+            self.tableView.reloadData()
         }))
         
         changeLocPrompt.addTextFieldWithConfigurationHandler ( { ( textField: UITextField!) in
@@ -412,15 +409,131 @@ class ViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegat
             
         })
         presentViewController(changeLocPrompt, animated: true, completion: nil)
-
+        
         locationManager.startUpdatingLocation()
     }
     
-}
-    class GalleryNavigationController: UINavigationController {
-        override func preferredStatusBarStyle() -> UIStatusBarStyle {
-            return .LightContent
+    
+    func data_request2() {
+        
+        let url1 = String("https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(self.userLoc2)&type=art_gallery&key=AIzaSyDNopD2lCPhs0z-Uap3f8EPUt9R3gGjGjg")
+        let url2 = String("https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(self.userLoc2)&type=art_gallery&key=AIzaSyDNopD2lCPhs0z-Uap3f8EPUt9R3gGjGjg&pagetoken=CqQDoAEAAHsw7tThV1V22yk57l00EASB3lYL9ANG0Zhi287TWYStsLLP2jMJjXIdsY41Pi3fTBvmrsoK1v_0-CfeHZkmGT5fHeHIEcTEj5kYsR1_uYqxooZVul1s7iFOzqzKMKz089JOpKNvedao71Oku_qBtaiJ25bmTF2laXsfAbrXH3sHi3CsdKdQiT8xo-bFgDiZlEGIBGlso3HM5YY5E2Wsg54uMYKU4_2RbD-xJVhl6JAobW8cn4mqe7UwAt8g3Iv0SxxUKuP8mOTcZXo60EfQ5snqXaNvWzy2yxcDbBtff6FTnjNuqYIOhVNg7SF0eyRZv2zcgbuU29WkyjZHUp5RqTyycZ-S6WyBCFv1GvcNy9TGf83VUzq6uZBxN6dEYOv35R9SIJ5NNNjetO97CnLNqJcXhC4JjLLRuwJUbGati2ZN5SKrIxGdeQSxkf7OETrQ81JQAkVzfD1Ap-Z7R3_lq7nNz_4vX9vXt-w9iIsGFQnXB0wUz_ODjtHz-_fcLl2ErgSj-sKdBZ5h2jLFlLcUscxjMhsLkgniJg6CmpWCCAx2EhAf0iT0qNwxOo02HZp0HED6GhQqM42r8FUYp8QIN4ynhH6SgG0dAA")
+        let url3 = String("https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(self.userLoc2)&type=art_gallery&key=AIzaSyDNopD2lCPhs0z-Uap3f8EPUt9R3gGjGjg&pagetoken=CvQB4QAAAN97q78lv5-0k4ymrAN634vgCQ4ZRpsi9Irq0GWm0Wsa5_jP2mnIPo_S1mMJHYTRuhBESfjgdrjfm43VnQhzXodXbeIfXTWb0Z1fYIL9cRIbGWMLHc-CI--fMDDbHuiOPK3M9W2drdb0lBrOWx2B0mgAaqNGc4H6PnGJ8wBYdp5ulc-6w5G1Obm1ai7pMECABc_AOVVSXZSqFHyPJ-oytt9kA54Z60NbuJyl86KBLSELhDMZYfPez6Z84zUZB-qmZyEws4t64i5SeH_WF4QszlRJsG86S9JnI-3tX-pBqyPD8DicdFYWbNFx_0OoRG6zARIQPNcliuomX5_JWRt5Ys1FNRoUEx8nzZJdYx7iY11qji1L8YOhkg4")
+        let url4 = String("https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(self.userLoc2)&type=art_gallery&key=AIzaSyDNopD2lCPhs0z-Uap3f8EPUt9R3gGjGjg&pagetoken=CvQB4QAAAJKqgaK9mciWANUddEgGhV0KhxJgvI03a2o8cuAUj_ftwjYbkEjn_woV79NpzMzQvhLZ7KcYO14M77oTUS5jZnHPse6ZQGvwig0cx9fKmhr7_sRqLzZO2zlMEtQgSwkuav7eAuPLbThj33xakabHgHMKFOaJlS1WSdTvEi1zTgJ2sZoO3GAwmjWjBrTIIWaL0kUPCpjwnAQlTEvg--lU2qNwdpeqwKAyIRy67NcVqhB_RguFTExbO7RKDgR39FT78Un5QySHFoM3eGt8zOK_COH6OUZW19mil70iXD9E1pARuxpwxpyCJE-cLHYqoVaGfBIQO7cdEarYOCT9f6YM6WBzQBoUPvkEpUtUHJEwlJsCML8DL7QFm1o")
+        
+        
+        let urlOne = NSURL(string: url1)
+        let urlTwo = NSURL(string: url2)
+        let urlThree = NSURL(string: url3)
+        let urlFour = NSURL(string: url4)
+        
+        let session1 = NSURLSession.sharedSession()
+        let session2 = NSURLSession.sharedSession()
+        let session3 = NSURLSession.sharedSession()
+        let session4 = NSURLSession.sharedSession()
+        
+        let task = session1.dataTaskWithURL(urlOne!) { (data , response, error ) -> Void in
+            do {
+                print(error)
+                let jsonDict = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                    as! NSDictionary
+                      print(jsonDict)
+                let galleries1 = jsonDict ["results"] as! [NSDictionary]
+                for dictionary in galleries1 {
+                    let galleryObject:Gallery = Gallery(galleryDictionary: dictionary)
+                    if !self.galleryArray.contains(galleryObject) {
+                        self.galleryArray.append(galleryObject)
+                        
+                    }
+                }
+                dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                    
+                    self.tableView.reloadData()
+                }
+            }
+                
+            catch let error as NSError {
+                print("jsonError: \(error.localizedDescription)")
+            }
         }
+        task.resume()
+        
+        let task2 = session2.dataTaskWithURL(urlTwo!) { (data , response, error ) -> Void in
+            do {
+                print(error)
+                let jsonDict = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                    as! NSDictionary
+                // print(jsonDict)
+                let galleries2 = jsonDict ["results"] as! [NSDictionary]
+                for dictionary in galleries2 {
+                    let galleryObject:Gallery = Gallery(galleryDictionary: dictionary)
+                    if !self.galleryArray.contains(galleryObject) {
+                        self.galleryArray.append(galleryObject)
+                    }
+                }
+                dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                    self.tableView.reloadData()
+                }
+            }
+            catch let error as NSError {
+                print("jsonError: \(error.localizedDescription)")
+                
+            }
+        }
+        task2.resume()
+        
+        let task3 = session3.dataTaskWithURL(urlThree!) { (data , response, error ) -> Void in
+            do {
+                print(error)
+                let jsonDict = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                    as! NSDictionary
+                //       print(jsonDict)
+                let galleries3 = jsonDict ["results"] as! [NSDictionary]
+                for dictionary in galleries3 {
+                    let galleryObject:Gallery = Gallery(galleryDictionary: dictionary)
+                    if !self.galleryArray.contains(galleryObject) {
+                        self.galleryArray.append(galleryObject)
+                    }
+                }
+                dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                    self.tableView.reloadData()
+                }
+            }
+            catch let error as NSError {
+                print("jsonError: \(error.localizedDescription)")
+                
+            }
+        }
+        
+        task3.resume()
+        
+        let task4 = session4.dataTaskWithURL(urlFour!) { (data , response, error ) -> Void in
+            do {
+                print(error)
+                let jsonDict = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                    as! NSDictionary
+                //   print(jsonDict)
+                let galleries4 = jsonDict ["results"] as! [NSDictionary]
+                for dictionary in galleries4 {
+                    let galleryObject:Gallery = Gallery(galleryDictionary: dictionary)
+                    self.galleryArray.append(galleryObject)
+                }
+                dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                    self.tableView.reloadData()
+                }
+            }
+            catch let error as NSError {
+                print("jsonError: \(error.localizedDescription)")
+            }
+        }
+        task4.resume()
+    }
+    
+}
+class GalleryNavigationController: UINavigationController {
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
 }
 
 
