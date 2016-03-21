8f5b7da3ac6c4ed0e972fca12a990f52660f808e
mouerj <joseph.mouer@icloud.com>
2016-03-20 21:30:01 -0400

    added user defined location VC

---
+++ GalleryandMuseumFinder/UserPickViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserPickViewController.swift
+//  GalleryandMuseumFinder
+//
+//  Created by Joseph Mouer on 3/20/16.
+//  Copyright © 2016 Mobile Makers. All rights reserved.
+//
+
+import UIKit
+
+class UserPickViewController: UIViewController {
+
+    @IBOutlet weak var userPickLoc: UITextField!
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+    
+
+  
+
+}
